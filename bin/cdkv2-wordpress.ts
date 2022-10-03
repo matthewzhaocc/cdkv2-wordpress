8525a00c4948fcec20048b45027dc74b0599e88c
Matthew Zhao <matthewzhaocc@gmail.com>
2022-10-02 20:24:13 -0700

    CDK NAG

--- bin/cdkv2-wordpress.ts
+++ bin/cdkv2-wordpress.ts
@@ -4,4 +4,9 @@ import * as cdk from "aws-cdk-lib";
 import { Cdkv2WordpressStack } from "../lib/cdkv2-wordpress-stack";
 
 const app = new cdk.App();
-new Cdkv2WordpressStack(app, "Cdkv2WordpressStack", {});
+new Cdkv2WordpressStack(app, "Cdkv2WordpressStack");
+
+import { Aspects } from "aws-cdk-lib";
+import { HIPAASecurityChecks } from "cdk-nag";
+
+Aspects.of(app).add(new HIPAASecurityChecks());
